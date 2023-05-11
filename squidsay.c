97cd0033da4dd4282644cfab02c927a7d1e4dd9e
mason1920 <mason1920@protonmail.com>
2023-05-11 18:04:29 -0400

    REMOVE ARGUMENT INPUT AND REMOVE LINE LIMITS
    REWORKED INTERNALS TO BE MORE CORRECT.

--- squidsay.c
+++ squidsay.c
@@ -10,139 +10,195 @@
  *        / | \
  */
 
+#include <ctype.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
-// Maximum amounts of text in bytes
-#define MAX_LINES 256
-#define MAX_LINE_LEN 256
+// `getdelim` with optional delimiter inclusion and
+// predicates that dictate what characters to strip from each side.
+//
+// Returns -1 on failure, the cause of which can be deduced by checking
+// the EOF status of `file`, otherwise,
+// by checking the error of `file`, or if none of the previous apply,
+// by checking `errno`.
+//
+// On success, returns the
+// count of characters in `*line` excluding the `'\0'` terminator
+// and sets `*line` and `*line_capacity` as described.
+static ssize_t
+getdelim_stripped(
+        // `NULL` or pointer to buffer of size `*line_capacity`.
+        // The pointer must be able to be passed to `free`.
+        // `'\0'` terminated on successful output.
+        char** line,
+        // Pointer to the capacity of `*line`.
+        // `*line_capacity` will not be read if `line` is `NULL`.
+        // `*line_capacity` is updated to reflect the capacity of `*line`
+        // as it changes during this procedure.
+        size_t* line_capacity,
+        int delimiter,
+        FILE* file,
+        int (*remove_left)(int),
+        int (*remove_right)(int),
+        // Whether or not to include `delimiter` at the end of `*line` and in the returned size if it was found.
+        bool keep_delimiter)
+{
+    size_t line_count = 0;
 
-// What the inputted text is stored in
-typedef char Text[MAX_LINES * MAX_LINE_LEN];
+    int character = getc(file);
 
-// A formatted list of the lines of the text and the longest line
-typedef struct {
-    char lines_arr[MAX_LINES][MAX_LINE_LEN];
-    int lines_len;
-    int max_len;
-} Lines;
+    if (character == EOF) return -1;
 
-// Takes input from CLI arguments
-int
-input_arg_text(Text text, const int argc, const char *argv[]) {
-    for (int i = 1; i < argc; ++i) {
-        strcat(text, argv[i]);
-        if (i + 1 != argc) {
-            const char *SPACE = " ";
-            strcat(text, SPACE);
+    if (!*line) {
+        void* buffer = malloc(100);
+        if (!buffer) {
+            ungetc(character, file);
+            return -1;
         }
+        *line = buffer;
+        *line_capacity = 100;
     }
-    return strlen(text);
+
+    // Strip left:
+    while (character != EOF && character != delimiter && (*remove_left)(character)) 
+        character = getc(file);
+
+    // Collect characters:
+    while (character != EOF && character != delimiter) {
+        // Grow:
+        if (line_count == *line_capacity) {
+            void* buffer = realloc(*line, *line_capacity + 100);
+            if (!buffer) {
+                ungetc(character, file);
+                return -1;
+            }
+            *line = buffer;
+            *line_capacity += 100;
+        }
+
+        (*line)[line_count++] = character;
+        character = getc(file);
+    }
+
+    // Strip right:
+    while (line_count > 0 && (*remove_right)((*line)[line_count - 1]))
+        line_count -= 1;
+
+    // Resize `*line` to be just enough to fit
+    // the delimiter if desired and found
+    // and `'\0'`:
+
+    keep_delimiter = keep_delimiter && character == delimiter;
+
+    size_t size = line_count + (keep_delimiter ? 2 : 1);
+
+    void* buffer = realloc(*line, size);
+    if (buffer)
+        *line_capacity = size;
+    else if (size > *line_capacity) 
+        return -1;
+    *line = buffer;
+
+    if (keep_delimiter) (*line)[line_count++] = delimiter;
+    (*line)[line_count] = '\0';
+
+    return line_count;
 }
 
-// Takes input from CLI stdin
-int
-input_stdin_text(Text text) {
-    int c, len = 0;
-    while ((c = getchar()) != EOF) {
-        text[len++] = c;
-    }
-    return len;
-}
+struct FatChar {
+    char* buffer;
+    size_t count;
+};
 
-// Decides whether to take input from arguments or stdin, and does that
-void
-input_text(Text text, const int argc, const char *argv[]) {
-    int len;
-    if (argc > 1) {
-        len = input_arg_text(text, argc, argv);
-    } else {
-        len = input_stdin_text(text);
-    }
-    if (text[len - 1] == '\n') {
-        text[--len] = '\0';
-    }
-    text[len] = '\0';
-}
+static struct FatChar
+getcontents(struct FatChar line)
+{
+    ssize_t count = getdelim_stripped(
+            &line.buffer,
+            &line.count,
+            '\n',
+            stdin,
+            &isspace,
+            &isspace,
+            false);
 
-// Copy string to string, but with every tab replaced with a number of spaces, and returns length
-int
-detab_strcpy(char dest[], const char src[]) {
-    int len = 0, i = 0;
-    while ((dest[len] = src[i++])) {
-        if (dest[len] == '\t') {
-            dest[len] = '\0';
-            const char *TAB_SPACE = "    ";
-            strcat(dest, TAB_SPACE);
-            len += strlen(TAB_SPACE);
-        } else {
-            ++len;
-        }
-    }
-    return len;
-}
+    if (count == -1)
+        return (struct FatChar){ NULL, 0 };
 
-// Formats the given text into a list of lines
-void
-parse_lines(Lines *lines, Text text) {
-    char *curr_line = text;
-    while (curr_line) {
-        char *next_line = strchr(curr_line, '\n');
-        if (next_line) {
-            *next_line = '\0';
-        }
-        printf("%s", lines->lines_arr[lines->lines_len]);
-        int len = detab_strcpy(lines->lines_arr[lines->lines_len], curr_line);
-        if (len > lines->max_len) {
-            lines->max_len = len;
-        }
-        if (next_line) {
-            *next_line = '\n';
-        }
-        curr_line = next_line ? next_line + 1 : NULL;
-        ++(lines->lines_len);
-    }
-}
-
-// Prints the text in a speech bubble
-void
-print_speech(Text text) {
-    Lines lines = { 0 };
-    parse_lines(&lines, text);
-    printf(" ");
-    for (int i = 0; i < lines.max_len + 2; ++i) {
-        printf("_");
-    }
-    printf("\n");
-    for (int i = 0; i < lines.lines_len; ++i) {
-        printf("| %s", lines.lines_arr[i]);
-        for (int j = 0, len = strlen(lines.lines_arr[i]); j < lines.max_len - len; ++j) {
-            printf(" ");
-        }
-        printf(" |\n");
-    }
-    printf(" ");
-    for (int i = 0; i < lines.max_len + 2; ++i) {
-        printf("-");
-    }
-}
-
-// Draws the text, speech bubble, and Squidward himself
-void
-draw_squid(Text text) {
-    print_speech(text);
-    printf("\n    \\   ___");
-    printf("\n     \\ (.. )");
-    printf("\n       -|-");
-    printf("\n       /| |\\");
-    printf("\n        | |");
-    printf("\n       / | \\\n");
+    return (struct FatChar){ line.buffer, count };
 }
 
 int
-main(const int argc, const char *argv[]) {
-    Text text;
-    input_text(text, argc, argv);
-    draw_squid(text);
-    return 0;
+main(void)
+{
+    struct FatChar* lines = NULL;
+    size_t lines_count = 0;
+    size_t lines_capacity = 0;
+
+    size_t line_longest = 0;
+
+    while (true) {
+        struct FatChar line = getcontents((struct FatChar){ NULL, 0 });
+
+        if (!line.buffer) {
+            if (feof(stdin)) {
+                // Draw top border:
+                putchar(' ');
+                for (size_t i = 0; i < line_longest + 2; ++i)
+                    putchar('_');
+                putchar('\n');
+
+                // Draw contents:
+                for (size_t line = 0; line < lines_count; ++line) {
+                    fputs("| ", stdout);
+                    fputs(lines[line].buffer, stdout);
+                    for (size_t i = 0; i < line_longest - lines[line].count; ++i)
+                        putchar(' ');
+                    puts(" |");
+                }
+
+                // Draw bottom border:
+                putchar(' ');
+                for (size_t i = 0; i < line_longest + 2; ++i)
+                    putchar('-');
+
+                // Draw squid:
+                puts("\n\
+    \\   ___\n\
+     \\ (.. )\n\
+       -|-\n\
+       /| |\\\n\
+        | |\n\
+       / | \\");
+
+                return 0;
+            }
+
+            int error = ferror(stdin);
+            if (error) return error;
+
+            perror(NULL);
+            return errno;
+        }
+
+        if (line.count > line_longest) line_longest = line.count;
+
+        // Grow:
+        if (lines_count == lines_capacity) {
+            size_t new_capacity = lines_capacity + 100 * sizeof(struct FatChar);
+
+            void* buffer = realloc(lines, new_capacity);
+            if (!buffer) {
+                perror(NULL);
+                return errno;
+            }
+
+            lines = buffer;
+            lines_capacity = new_capacity;
+        }
+
+        lines[lines_count++] = line;
+    }
 }
